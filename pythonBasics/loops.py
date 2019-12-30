3c135023a52a5d5a407b042b11067bf47087b88f
Sanjay Singh Bisht <sanjaysinghbisht114@gmail.com>
2019-12-30 21:11:45 +0530

    Done revision of loops

---
+++ pythonBasics/loops.py
@@ -0,0 +1,21 @@
+i = -1
+# while i < 11:
+#     i += 1
+#     if i == 4:
+#         continue
+#     print(i, end=' ')
+
+# for i in "Hi how are ML":
+#     print(i, end=' ')
+
+
+for i in ["Sumit", "Sanjay", "Harsh", "Himanshu", "Shubham"]:
+    print(i, end=' ')
+print(len(["Sumit", "Sanjay", "Harsh", "Himanshu", "Shubham"]))
+
+
+
+
+
+
+
